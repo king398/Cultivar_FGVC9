fa26e9855dae22765d0ef1d85e2170e1fe4f88db
king398 <mithilsalunkhe@outlook.com>
2022-03-12 21:05:23 +0530

    Testing Methods

--- notebooks/test_sub.py
+++ notebooks/test_sub.py
@@ -1,3 +1,8 @@
 id = 'abfmneahjp'
-labels = {'abfmneahjp_p0': 1, "abfmneahjp_p1": 0, 'abfmneahjp_p2': 0}
-
+labels = {'abfmneahjp_p0': 1, "abfmneahjp_p1": 0, 'abfmneahjp_p2': 0, "meow_123": 3}
+possible_values = list(map(lambda value: f"{id}_p{value}", range(10)))
+labels_list = []
+for id, label in labels.items():
+	if id in possible_values:
+		labels_list.append(label)
+print(sum(labels_list))

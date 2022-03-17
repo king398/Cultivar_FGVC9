95348b9b1e6d0f5fff5130bcba25c1126a316b49
king398 <mithilsalunkhe@outlook.com>
2022-03-17 20:40:33 +0530

    Smol change

--- notebooks/model.py
+++ notebooks/model.py
@@ -142,7 +142,7 @@ class BaseModel(nn.Module):
 		self.model = timm.create_model(self.cfg['model'], pretrained=self.cfg['pretrained'],
 		                               in_chans=self.cfg['in_channels'],
 		                               num_classes=100)
-		n_features = self.model.fc.in_features
+		n_features = self.model.classifier.in_features
 		self.model.head = nn.Linear(n_features, cfg['target_size'])
 
 	def forward(self, x):

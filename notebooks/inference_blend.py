c816fd126dce9558dc9d5ba131293986b3132dd3
mithil <mithilsalunkhe@outlook.com>
2022-03-24 11:10:24 +0530

    Added Blend pipeline

---
+++ notebooks/inference_blend.py
@@ -0,0 +1,90 @@
+######## Helper Functions #############
+from tqdm.auto import tqdm
+from collections import defaultdict
+import pandas as pd
+import os
+import random
+import gc
+import yaml
+from pathlib import Path
+import argparse
+import numpy as np
+import random
+import glob
+
+from sklearn import preprocessing
+############# Deep learning Stuff #################
+import torch
+from torch.nn import functional as F
+from torch import nn
+import torch.optim as optim
+####### Function Created by me ###############
+from utils import *
+from augmentations import *
+from dataset import *
+from model import *
+from train_func import *
+
+
+def main(cfg):
+    train_df = pd.read_csv(cfg['train_file_path'])
+    probabilitys = None
+    probabilitys2 = None
+    seed_everything(cfg['seed'])
+    gc.enable()
+    device = return_device()
+    label_encoder = preprocessing.LabelEncoder()
+    train_df['cultivar'] = label_encoder.fit_transform(train_df['cultivar'])
+    paths = glob.glob(f"{cfg['test_dir']}/*.jpeg")
+    test_dataset = Cultivar_data_inference(image_path=paths,
+                                           transform=get_test_transforms(cfg['image_size']))
+
+    test_loader = DataLoader(
+        test_dataset, batch_size=cfg['batch_size'], shuffle=False,
+        num_workers=cfg['num_workers'], pin_memory=cfg['pin_memory']
+    )
+    ids = list(map(lambda string: string.split('/')[-1], paths))
+    ids = list(map(lambda string: string.split('.')[0], ids))
+    ids = list(map(lambda string: string + '.png', ids))
+
+    for path in glob.glob(f"{cfg['model_path']}/*.pth"):
+        model = BaseModelEffNet(cfg)
+        model.load_state_dict(torch.load(path))
+        model.to(device)
+        model.eval()
+        probablity = inference_fn(test_loader, model, cfg)
+
+        if probabilitys is None:
+            probabilitys = probablity / 5
+        else:
+            probabilitys += probablity / 5
+        del model
+        gc.collect()
+        torch.cuda.empty_cache()
+    for path in glob.glob(f"{cfg['model_path2']}/*.pth"):
+        model = BaseModelEffNet(cfg)
+        model.load_state_dict(torch.load(path))
+        model.to(device)
+        model.eval()
+        probablity = inference_fn(test_loader, model, cfg)
+
+        if probabilitys is None:
+            probabilitys2 = probablity / 5
+        else:
+            probabilitys2 += probablity / 5
+        del model
+        gc.collect()
+        torch.cuda.empty_cache()
+    probabilitys = probabilitys * 0.6 + probabilitys2 * 0.4
+    preds = torch.argmax(probabilitys, 1).numpy()
+    sub = pd.DataFrame({"filename": ids, "cultivar": label_encoder.inverse_transform(preds)})
+    sub.to_csv(cfg['submission_file'], index=False)
+
+
+if __name__ == '__main__' and '__file__' in globals():
+    parser = argparse.ArgumentParser(description='Baseline')
+    parser.add_argument("--file", type=Path)
+    args = parser.parse_args()
+    with open(str(args.file), "r") as stream:
+        cfg = yaml.safe_load(stream)
+    main(cfg)
--- notebooks/train_func.py
+++ notebooks/train_func.py
@@ -4,74 +4,76 @@ from torch.cuda.amp import autocast
 from utils import *
 import numpy as np
 import gc
+
+
 def train_fn(train_loader, model, criterion, optimizer, epoch, cfg, scheduler=None):
-	device = torch.device(cfg['device'])
-	metric_monitor = MetricMonitor()
-	model.train()
-	stream = tqdm(train_loader)
-	for i, (images, target) in enumerate(stream, start=1):
-		images = images.to(device, non_blocking=True)
+    device = torch.device(cfg['device'])
+    metric_monitor = MetricMonitor()
+    model.train()
+    stream = tqdm(train_loader)
+    for i, (images, target) in enumerate(stream, start=1):
+        images = images.to(device, non_blocking=True)
 
-		target = target.to(device, non_blocking=True).long()
+        target = target.to(device, non_blocking=True).long()
 
-		with autocast():
-			output = model(images)
-		loss = criterion(output, target)
+        with autocast():
+            output = model(images)
+        loss = criterion(output, target)
 
-		accuracy = accuracy_score(output, target)
+        accuracy = accuracy_score(output, target)
 
-		metric_monitor.update("Loss", loss.item())
-		metric_monitor.update("Accuracy", accuracy)
-		loss.backward()
-		optimizer.step()
-		if scheduler is not None:
-			scheduler.step()
+        metric_monitor.update("Loss", loss.item())
+        metric_monitor.update("Accuracy", accuracy)
+        loss.backward()
+        optimizer.step()
+        if scheduler is not None:
+            scheduler.step()
 
-		optimizer.zero_grad()
-		stream.set_description(f"Epoch: {epoch:02}. Train. {metric_monitor}")
+        optimizer.zero_grad()
+        stream.set_description(f"Epoch: {epoch:02}. Train. {metric_monitor}")
 
 
 def validate_fn(val_loader, model, criterion, epoch, cfg):
-	device = torch.device(cfg['device'])
-	metric_monitor = MetricMonitor()
-	model.eval()
-	stream = tqdm(val_loader)
-	accuracy_list = []
-	with torch.no_grad():
-		for i, (images, target) in enumerate(stream, start=1):
-			images = images.to(device, non_blocking=True)
+    device = torch.device(cfg['device'])
+    metric_monitor = MetricMonitor()
+    model.eval()
+    stream = tqdm(val_loader)
+    accuracy_list = []
+    with torch.no_grad():
+        for i, (images, target) in enumerate(stream, start=1):
+            images = images.to(device, non_blocking=True)
 
-			target = target.to(device, non_blocking=True).long()
-			with autocast():
-				output = model(images)
-			loss = criterion(output, target)
+            target = target.to(device, non_blocking=True).long()
+            with autocast():
+                output = model(images)
+            loss = criterion(output, target)
 
-			accuracy = accuracy_score(output, target)
-			metric_monitor.update("Loss", loss.item())
-			metric_monitor.update("Accuracy", accuracy)
-			stream.set_description(f"Epoch: {epoch:02}. Valid. {metric_monitor}")
-			accuracy_list.append(accuracy)
-	return np.mean(accuracy_list)
+            accuracy = accuracy_score(output, target)
+            metric_monitor.update("Loss", loss.item())
+            metric_monitor.update("Accuracy", accuracy)
+            stream.set_description(f"Epoch: {epoch:02}. Valid. {metric_monitor}")
+            accuracy_list.append(accuracy)
+    return np.mean(accuracy_list)
 
 
 def inference_fn(test_loader, model, cfg):
-	device = torch.device(cfg['device'])
-	model.eval()
-	stream = tqdm(test_loader)
-	preds = None	
-	with torch.no_grad():
-		for i, images in enumerate(stream, start=1):
-			images = images.to(device, non_blocking=True)
-						
-			with autocast():
-				output = model(images)
+    device = torch.device(cfg['device'])
+    model.eval()
+    stream = tqdm(test_loader)
+    preds = None
+    with torch.no_grad():
+        for i, images in enumerate(stream, start=1):
+            images = images.to(device, non_blocking=True)
 
-			pred = torch.softmax(output, 1).detach().cpu()
-			if preds is None:
-				preds = pred
-			else:
-				preds = torch.cat((preds, pred))
-			del pred
-			gc.collect()
+            with autocast():
+                output = model(images)
 
-	return preds
+            pred = torch.softmax(output, 1).detach().cpu()
+            if preds is None:
+                preds = pred
+            else:
+                preds = torch.cat((preds, pred))
+            del pred
+            gc.collect()
+
+    return preds

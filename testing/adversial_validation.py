b4fa83fe50f8e35e6bd9c4e3f9368b66ff8e42d7
king398 <mithilsalunkhe@outlook.com>
2022-03-27 12:07:38 +0530

    Updated Num Classes

--- testing/adversial_validation.py
+++ testing/adversial_validation.py
@@ -17,22 +17,22 @@ path = Path('../input/small-jpegs-fgvc')
 device = torch.device("cuda:0")
 
 
-def return_filpath(name, folder='../input/small-jpegs-fgvc/train/'):
+def return_filpath(name, folder='/home/mithil/PycharmProjects/Cultivar_FGVC9/data/archive/train/'):
     path = os.path.join(folder, f'{name}')
     return path
 
 
-def return_filepath_id(name, folder='../input/small-jpegs-fgvc/test/'):
+def return_filepath_id(name, folder='/home/mithil/PycharmProjects/Cultivar_FGVC9/data/archive/test/test/'):
     name = name.split('.')[0]
     path = os.path.join(folder, f'{name}.jpeg')
     return path
 
 
-train_df = pd.read_csv('../input/small-jpegs-fgvc/train_cultivar_mapping.csv')
+train_df = pd.read_csv('/home/mithil/PycharmProjects/Cultivar_FGVC9/data/archive/train_cultivar_mapping.csv')
 train_ids = train_df['image'].values
 train_path = list(map(return_filpath, train_ids))
 train_labels = np.zeros(len(train_ids))
-test_df = pd.read_csv('../input/small-jpegs-fgvc/sample_submission.csv')
+test_df = pd.read_csv('/home/mithil/PycharmProjects/Cultivar_FGVC9/data/archive/sample_submission.csv')
 test_ids = test_df['filename'].values
 test_path = list(map(return_filepath_id, test_ids))
 test_labels = np.ones(len(test_path))
@@ -40,4 +40,8 @@ df = pd.DataFrame.from_dict(
     {'path': np.concatenate((train_path, test_path)), 'label': np.concatenate((train_labels, test_labels))})
 df = df.sample(frac=1).reset_index(drop=True)
 df.head()
-dls = ImageDataLoaders.from_df(df, path)
+dls = ImageDataLoaders.from_df(df)
+learn = cnn_learner(dls, models.resnet34, metrics=AccumMetric(func=RocAucBinary))
+print(learn.loss_func)
+lr = learn.lr_find()
+learn.fit_one_cycle(1, lr_max=lr)

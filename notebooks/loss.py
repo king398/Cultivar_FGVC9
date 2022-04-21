7e864a491285827707d63274b4d3f19e15d28edb
king398 <mithilsalunkhe@outlook.com>
2022-04-21 16:06:43 +0530

    Cfg change

--- notebooks/loss.py
+++ notebooks/loss.py
@@ -79,3 +79,18 @@ class AsymmetricLossOptimized(nn.Module):
             self.loss *= self.asymmetric_w
 
         return -self.loss.sum()
+
+
+def loss_fn_kd(outputs, labels, teacher_outputs, params):
+    """
+    Compute the knowledge-distillation (KD) loss given outputs, labels.
+    "Hyperparameters": temperature and alpha
+    NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
+    and student expects the input tensor to be log probabilities! See Issue #2
+    """
+    alpha = params.alpha
+    T = params.temperature
+    KD_loss = nn.KLDivLoss()(F.log_softmax(outputs / T, dim=1),
+                             F.softmax(teacher_outputs / T, dim=1)) * (alpha * T * T) + F.cross_entropy(outputs, labels) * (1. - alpha)
+
+    return KD_loss

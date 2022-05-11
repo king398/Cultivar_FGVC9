3d765336a7d54006dc39e484afa7088ea1cdd940
king398 <mithilsalunkhe@outlook.com>
2022-05-11 08:04:48 +0530

    label smoothing

--- notebooks/blend.py
+++ notebooks/blend.py
@@ -9,20 +9,20 @@ label_encoder.fit(train_csv['cultivar'])
 
 effnet = torch.tensor(
     np.load(
-        '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/tta/effnetv2_b3_CLAHE_augs_640_7xtta_probablity.npy'))
+        '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/tta/effnetv2_m_25epoch_probablity.npy'))
 
 effnet_big = torch.tensor(
     np.load(
-    '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/tta/tf_effnetv2_b3_cutmix_35_own_tta_meanstd_640_probablity.npy',
+        '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/tta/seresnext_50_25epoch_probablity.npy',
         allow_pickle=True),
 )
 
 ids = pd.read_csv(
     '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/tf_efficientnet_b3_ns_mixup_more_epoch_tta_submission.csv')[
     'filename']
-probablity = effnet * 0.5 + effnet_big * 0.5
+probablity = effnet * 0.65 + effnet_big * 0.35
 preds = torch.argmax(probablity, 1).numpy()
 sub = pd.DataFrame({"filename": ids, "cultivar": label_encoder.inverse_transform(preds)})
 sub.to_csv(
-    '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/blend/effnetv2_b3_diffmean_augs_640_7x_probablity_tf_effnetv2_b3_cutmix_35_own_tta_640_probablity.csv',
+    '/home/mithil/PycharmProjects/Cultivar_FGVC9/submissions/blend/seresnext_50_25epoch_probablity__effnetv2_m_25epoch_probablity.csv',
     index=False)
--- notebooks/train.py
+++ notebooks/train.py
@@ -54,7 +54,7 @@ def main(cfg):
 
             model.to(device)
 
-            criterion = nn.CrossEntropyLoss()
+            criterion = nn.CrossEntropyLoss(label_smoothing=0.2)
 
             optimizer = eval(cfg['optimizer'])(model.parameters(), lr=float(cfg['lr']))
             train_dataset = Cultivar_data(image_path=train_path,
